92eb3f8f40ec1b0a70238eed73e734068b434f84
Val Bob <val.bob@example.com>
2022-04-05 10:42:56 +0200

    Add and setup login_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -2,5 +2,12 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")  # tuple to be passed as args (how, what)
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")  # tuple to be passed as args (how, what)
+
+
+class LoginPageLocators:
+    LOGIN_URL = (By.CSS_SELECTOR, "#login_link")  # TODO: change selector value
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")  # TODO: change selector value
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")  # TODO: change selector value
+
 
