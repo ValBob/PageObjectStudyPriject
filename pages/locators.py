b01f7ecca39bb7d40e5512ddffe2671602912546
Val Bob <val.bob@example.com>
2022-04-07 14:26:10 +0200

    Add tests to check if basket is empty from main and product pages

--- pages/locators.py
+++ pages/locators.py
@@ -1,6 +1,17 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators:
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+class BasketPageLocators:
+    BASKET_ITEM = (By.CSS_SELECTOR, ".basket-items")
+    BASKET_EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner > p")
+
+
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")  # tuple to be passed as args (how, what)
 
